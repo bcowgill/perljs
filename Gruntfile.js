c3385ae7381cba65747686a0c2ae18b2a28b3070
Brent S.A. Cowgill <brent@blismedia.com>
2014-07-01 15:52:39 +0100

    package.json Gruntfile.js Makefile Added grunt-mocha-chai-sinon task runner to project to run unit tests in the console

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-/*jshint indent: 4, smarttabs: true, maxstatements: 100 */
+/*jshint indent: 4, smarttabs: true, maxstatements: 100, maxlen: 140 */
 /*global module:false */
 module.exports = function(grunt) {
 	'use strict';
@@ -34,6 +34,25 @@ module.exports = function(grunt) {
 		nodeunit: {
 			files: ['test/**/*-test.js'],
 		},
+		// run mocha/chai tests in console https://github.com/thepeg/grunt-mocha-chai-sinon
+		'mocha-chai-sinon': {
+			test: {
+				src: ['./test/**/*-test.js'],
+				options: {
+					ui: 'bdd',
+					// spec, list, tap, nyan, progress, dot, min, landing, doc, markdown, html-cov, json-cov, json, json-stream, xunit
+					reporter: 'tap',
+					bail: false, // true to bail after first test failure
+					//grep: '.*', // invert: true, // filter to run subset of tests
+					sort: true, // sort order of test files
+					trace: true, // trace function calls
+					'check-leaks': true, // check for global variable leaks
+					'expose-gc': true,
+					//timeout: 10, slow: 10, // async ms timeout and slow test threshold
+					'inline-diffs': false// show actual/expected diffs inline
+				}
+			}
+		},
 		// about jsdoc documentation tags: http://usejsdoc.org/
 		jsdoc : {
 			docs : {
@@ -67,17 +86,22 @@ module.exports = function(grunt) {
 
 	// These plugins provide necessary tasks.
 	//grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-mocha-chai-sinon');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-jsdoc');
 
 	// Default task.
 	grunt.registerTask('default', ['all']);
+	grunt.registerTask('test', [
+		//'nodeunit'
+		'mocha-chai-sinon'
+	]);
 	grunt.registerTask('all', [
 		'jshint:gruntfile',
 		'jshint:lib',
 		'jshint:test',
-		//'nodeunit',
+		'test',
 		'jsdoc'
 	]);
 	grunt.registerTask('single', ['jshint:single']);
--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	./node_modules/.bin/mocha --reporter nyan
+	mocha --reporter nyan
 
 docs:
 	rm -rf doc/
