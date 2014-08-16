5703970951f1300ad110ae9fcdefa6fd22a54197
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-08-16 19:43:34 +0100

    cygwin.sh adding paths and --congigure options to jsdoc command to match grunt configuration

--- scripts/cygwin.sh
+++ scripts/cygwin.sh
@@ -2,8 +2,8 @@
 # on my winpc with cygwin grunt-jsdoc not working so do by hand
 grunt windows
 echo Run tests
-mocha --reporter nyan test/
-#mocha --reporter spec test/
+#mocha --reporter nyan test/
+mocha --reporter spec test/
 echo Create jsdoc documentation
 rm -rf doc
-jsdoc --destination doc --recurse test/ lib/perl.js
+jsdoc --configure 'jsdoc.conf.json' --destination doc --recurse test/ lib/perl.js Gruntfile.js README.md
