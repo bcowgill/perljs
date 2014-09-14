0a767a3c4c832e9bb6d32974e76d8fb31ad2d38e
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-09-14 01:40:51 +0100

    Added .qwm() function for perlish quote words and create a hash map from it.
    Added _value() method to convert strings like true into boolean true when doing a qwm()
    Added _console property to allow custom console logging object for warnings.
    Added _valueMap object to allow definition of what strings map to values for qwm()
    Test cases added for _value() and all tests passing for qwm()

--- perl/perl-test.pl
+++ perl/perl-test.pl
@@ -9,4 +9,20 @@ use Test::More;
 
 my $var;
 
+print "q()\n";
+print "" . q() . "\n";
+
+print "qw()\n";
+print "@{[qw(one two three)]}\n";
+
+my %Map = qw(one two three four);
+print Dumper(\%Map);
+print "@{[keys(%Map)]}\n";
+print "@{[values(%Map)]}\n";
+
+my %MapOdd = qw(one two three);
+print Dumper(\%MapOdd);
+
 print "" . ($var x 12) . "\n";
+
+print "done\n";
