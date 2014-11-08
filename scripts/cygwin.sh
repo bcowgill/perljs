203815fe02ebb1b91359ddc8eaa929850afe3a01
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-11-08 18:52:28 +0000

    Experimenting with jsdoc - file and namespace documentation examples.
    perl.js - changed {String} to {string} etc

--- scripts/cygwin.sh
+++ scripts/cygwin.sh
@@ -4,8 +4,12 @@ grunt windows
 echo Run tests
 #mocha --reporter nyan test/
 mocha --reporter spec test/
+perl perl/perl-test.pl
+./scripts/check-ver.sh
+
+echo " "
 echo Create jsdoc documentation
 rm -rf doc
-jsdoc --configure 'jsdoc.conf.json' --destination doc --recurse test/ lib/perl.js Gruntfile.js README.md
-perl perl/perl-test.pl
-scripts/check-ver.sh
+jsdoc --configure 'jsdoc.conf.json' --destination doc --recurse test/ lib/perl.js lib/jsdoc-templates/ Gruntfile.js README.md
+
+echo Done
