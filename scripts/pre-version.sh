a05775a82ac8405e7648885eafdbceab2b364ad4
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-12-14 15:06:23 +0000

    pre/version scripts add exit on any error and update version number, build etc.

--- scripts/pre-version.sh
+++ scripts/pre-version.sh
@@ -2,12 +2,16 @@
 # pre npm version handler
 # https://docs.npmjs.com/cli/version
 
+# terminate on first error
+set -e
+# turn on trace of currently running command if you need it
+#set -x
+
 PREVER=`packagever.sh`
 if [ -z "$PREVER" ]; then
+	echo NOT OK getting version number
     exit 1
 fi
 
 # pre-release ensure tests all pass
 grunt preversion
-
-exit 1
--- scripts/version.sh
+++ scripts/version.sh
@@ -2,7 +2,45 @@
 # npm version handler after version number has been bumped
 # https://docs.npmjs.com/cli/version
 
+# terminate on first error
+set -e
+# turn on trace of currently running command if you need it
+#set -x
+
 VER=`packagever.sh`
 if [ -z "$VER" ]; then
+	echo NOT OK getting version number
     exit 1
 fi
+
+echo adding release note to README.md
+echo "* $VER " >> README.md
+$EDITOR README.md
+
+if grep "\* $VER" README.md ; then
+	echo ok README.md
+else
+	echo README.md does not contain a release note
+	exit 1
+fi
+git add README.md
+
+# Update the version numbers in some files
+perl -i.bak -pne 's{(\@version \s+)([\.0-9]+)}{$1$ENV{VER}}xmsg; \
+   s{(version \s* = \s*.)([\.0-9]+)(.;)}{$1$ENV{VER}$3}xmsg;' \
+   lib/perl.js
+
+perl -i.bak -pne 's{("version": \s+ ")([\.0-9]+)(",)}{$1$ENV{VER}$3}xmsg;' \
+   bower.json
+
+# Build documentation and minified distribution for web
+grunt all
+
+# pack the npm module and show it to check there are no extra files included
+npm pack && tar tvzf perljs-*.tgz
+rm perljs-*.tgz
+
+echo If there are files in then package which should not be, press Ctrl-C
+read prompt
+
+exit 1
