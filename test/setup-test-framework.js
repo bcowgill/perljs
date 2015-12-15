e200432a8c820c4165d5922405b91c65f1bf374a
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-12-15 18:04:11 +0000

    Failed attempt to set up karma testing. May revisit this again some other time.

--- test/setup-test-framework.js
+++ test/setup-test-framework.js
@@ -1,8 +1,10 @@
 // setup-test-framework.js - setup global test framework functions for testing in node and browser
 'use strict';
+console.log(new Date(), 'start setup-test-framework');
 
 /* jshint -W054 */ // The Function constructor is a form of eval.
 var isBrowser = new Function('try { return this === window; } catch (e) { return false; }');
+var isKarma = new Function('try { return "__karma__" in this; } catch (e) { return false; }');
 
 function setupTestFramework () {
     var getGlobal = new Function('return this;'),
@@ -11,10 +13,27 @@ function setupTestFramework () {
 
     root.chai = chai;
     root.should = chai.should();
+
     root.expect = chai.expect;
 }
 
-if (isBrowser()) {
+if (isKarma()) {
+    console.log(new Date(), 'isKarma setup-test-framework', typeof mocha);
+    require.config({
+        paths: {
+            'chai': '/base/node_modules/chai/chai'
+        }
+    });
+    require(['require', 'chai'], function (require) {
+        setupTestFramework();
+        // invokes the test-suite-browser.js
+        require(['require', 'test-suite-browser'], function () {
+            console.log(new Date(), 'setup-test-framework');
+        });
+    });
+}
+else if (isBrowser()) {
+    console.log(new Date(), 'isBrowser setup-test-framework');
     require.config({
         paths: {
             'chai': '../node_modules/chai/chai'
