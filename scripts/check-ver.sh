865e06d7f4a16fcbb521f127c5f9309c5085a1ba
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-02-24 15:26:48 +0000

    wip Improve check-ver.sh script to show only bersion numbers

--- scripts/check-ver.sh
+++ scripts/check-ver.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-grep version package.json lib/perl.js README.md
-tail README.md
+echo git config
+git config --list | grep user
+echo npm config
+npm config ls -l | grep author
+
+egrep 'version.+[0-9]' package.json lib/perl.js README.md
+if which after.sh > /dev/null; then
+	after.sh History < README.md
+else
+	tail README.md
+fi
