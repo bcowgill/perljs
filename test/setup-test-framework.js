50bbc4ca77d882fa1367089a3c2dab472824907d
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-02-24 17:13:45 +0000

    wip Configured jshint settings to match prettier settings and beautified everything.

--- test/setup-test-framework.js
+++ test/setup-test-framework.js
@@ -1,33 +1,36 @@
 // setup-test-framework.js - setup global test framework functions for testing in node and browser
-'use strict';
+'use strict'
 
-/* jshint -W054 */ // The Function constructor is a form of eval.
-var isBrowser = new Function('try { return this === window; } catch (e) { return false; }');
+// W054 is warning The Function constructor is a form of eval.
+/* jshint -W054 */
+var isBrowser = new Function(
+	'try { return this === window; } catch (e) { return false; }'
+)
 
-function setupTestFramework () {
-    var getGlobal = new Function('return this;'),
-        chai = require('chai'),
-        root = getGlobal();
+function setupTestFramework() {
+	var getGlobal = new Function('return this;'),
+		chai = require('chai'),
+		root = getGlobal()
+	/* jshint +W054 */
 
-    root.chai = chai;
-    root.should = chai.should();
-    root.expect = chai.expect;
+	root.chai = chai
+	root.should = chai.should()
+	root.expect = chai.expect
 }
 
 if (isBrowser()) {
-    require.config({
-        paths: {
-            'chai': '../node_modules/chai/chai'
-        }
-    });
-    require(['require', 'chai'], function (require) {
-        setupTestFramework();
-        // invokes the test-suite-browser.js
-        require(['require', 'test-suite-browser'], function () {
-            console.log(new Date(), 'setup-test-framework');
-        });
-    });
-}
-else {
-    setupTestFramework();
+	require.config({
+		paths: {
+			chai: '../node_modules/chai/chai',
+		},
+	})
+	require(['require', 'chai'], function (require) {
+		setupTestFramework()
+		// invokes the test-suite-browser.js
+		require(['require', 'test-suite-browser'], function () {
+			console.log(new Date(), 'setup-test-framework')
+		})
+	})
+} else {
+	setupTestFramework()
 }
--- test/test-suite-browser.js
+++ test/test-suite-browser.js
@@ -1,30 +1,30 @@
 // test-suite.js - in browser test suite runner
 
-'use strict';
+'use strict'
+
+// W054 is warning The Function constructor is a form of eval.
 /* global mocha, window */
 
-/* jshint -W054 */ // The Function constructor is a form of eval.
-var isBrowser = new Function('try { return this === window; } catch (e) { return false; }');
+/* jshint -W054 */
+var isBrowser = new Function(
+	'try { return this === window; } catch (e) { return false; }'
+)
 /* jshint +W054 */
 
 // modules which need to be pre-loaded for the tests
-var prerequisites = [
-    '../lib/perl'
-];
+var prerequisites = ['../lib/perl']
 
 // the tests to be run should be added here
-var specs = [
-    'perl-test.js'
-];
+var specs = ['perl-test.js']
 
 if (isBrowser()) {
-    require(['require'], function (require) {
-        require(prerequisites, function() {
-            require(specs, function() {
-                /* jshint maxcomplexity: 2 */
-                (window.mochaPhantomJS || mocha).run();
-                console.log(new Date(), 'test-suite-browser');
-            });
-        });
-    });
+	require(['require'], function (require) {
+		require(prerequisites, function () {
+			require(specs, function () {
+				/* jshint maxcomplexity: 2 */
+				;(window.mochaPhantomJS || mocha).run()
+				console.log(new Date(), 'test-suite-browser')
+			})
+		})
+	})
 }
