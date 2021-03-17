7a247f90fd9ebbe115fdb23a24c25e02fb4a9ffb
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-03-17 17:14:29 +0000

    wip Adding a post-publish script to test install the package.
    ready for final dry run test
    remove old release.sh script.
    package.json pre/post pubilsh actions finalised
    post-publish script checks published package version, installs it locally, runs the standalone test on it and then reenables post install whether it succeeds or fails.

---
+++ scripts/post-publish.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Do not run directly, npm will invoke this script at the right time.
+# post-publish script, checks the published npm version number is correct, installs the module and tests it.
+# https://docs.npmjs.com/cli/v7/commands/npm-version
+
+CMD=post-publish.sh
+PKG=$NPMPKG
+NPM=pnpm
+
+# terminate on first error
+set -e
+
+# turn on trace of currently running command if you need it
+#set -x
+
+echo $CMD handler: $* | tee --append local-git.log
+check-ver-lite.sh | tee --append local-git.log
+
+# on any exit, for husky we reenable the postinstall script
+function restore_pinst {
+	echo Finally, enable postinstall
+	pinst --enable
+}
+
+trap restore_pinst EXIT
+trap restore_pinst ERR
+
+REL_VER=`packagever.sh`
+if [ -z "$REL_VER" ]; then
+	echo NOT OK getting version number
+	exit 80
+fi
+
+sleep 3
+echo checking npm site
+check-published.sh $REL_VER
+
+$NPM info $PKG > packageinfo.txt
+less packageinfo.txt
+
+echo install and test package from npm registry
+rm -rf package || echo "ok no package dir"
+mkdir package
+echo "{
+  \"dependencies\": {
+    \"$PKG\": \"*\"
+  }
+}" > package/package.json
+pushd package
+	$NPM install
+popd
+./perl/js-test.js ../package/node_modules/$PKG/ $REL_VER
+
